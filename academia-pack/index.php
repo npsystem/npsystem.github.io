371a927f0ab65b6b09b55c97af197556e5771ea1
npsystem <npsystem.in@gmail.com>
2014-07-02 23:25:25 +0530

    index n vimeo

--- academia-pack/index.php
+++ academia-pack/index.php
@@ -1,3 +1,79 @@
+<?php
+require_once('vimeo.php');
+session_start();
+
+// Create the object and enable caching
+$vimeo = new phpVimeo('CONSUMER_KEY', 'CONSUMER_SECRET');
+$vimeo->enableCache(phpVimeo::CACHE_FILE, './cache', 300);
+
+// Clear session
+if ($_GET['clear'] == 'all') {
+    session_destroy();
+    session_start();
+}
+
+// Set up variables
+$state = $_SESSION['vimeo_state'];
+$request_token = $_SESSION['oauth_request_token'];
+$access_token = $_SESSION['oauth_access_token'];
+
+// Coming back
+if ($_REQUEST['oauth_token'] != NULL && $_SESSION['vimeo_state'] === 'start') {
+    $_SESSION['vimeo_state'] = $state = 'returned';
+}
+
+// If we have an access token, set it
+if ($_SESSION['oauth_access_token'] != null) {
+    $vimeo->setToken($_SESSION['oauth_access_token'], $_SESSION['oauth_access_token_secret']);
+}
+
+switch ($_SESSION['vimeo_state']) {
+    default:
+
+        // Get a new request token
+        $token = $vimeo->getRequestToken();
+
+        // Store it in the session
+        $_SESSION['oauth_request_token'] = $token['oauth_token'];
+        $_SESSION['oauth_request_token_secret'] = $token['oauth_token_secret'];
+        $_SESSION['vimeo_state'] = 'start';
+
+        // Build authorize link
+        $authorize_link = $vimeo->getAuthorizeUrl($token['oauth_token'], 'write');
+
+        break;
+
+    case 'returned':
+
+        // Store it
+        if ($_SESSION['oauth_access_token'] === NULL && $_SESSION['oauth_access_token_secret'] === NULL) {
+            // Exchange for an access token
+            $vimeo->setToken($_SESSION['oauth_request_token'], $_SESSION['oauth_request_token_secret']);
+            $token = $vimeo->getAccessToken($_REQUEST['oauth_verifier']);
+
+            // Store
+            $_SESSION['oauth_access_token'] = $token['oauth_token'];
+            $_SESSION['oauth_access_token_secret'] = $token['oauth_token_secret'];
+            $_SESSION['vimeo_state'] = 'done';
+
+            // Set the token
+            $vimeo->setToken($_SESSION['oauth_access_token'], $_SESSION['oauth_access_token_secret']);
+        }
+
+        // Do an authenticated call
+        try {
+            $videos = $vimeo->call('vimeo.videos.getUploaded');
+        }
+        catch (VimeoAPIException $e) {
+            echo "Encountered an API error -- code {$e->getCode()} - {$e->getMessage()}";
+        }
+
+        break;
+}
+
+?>
+
+
 <?php 
 /*
  * A Design by W3layouts
